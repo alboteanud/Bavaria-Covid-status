6e4c120064bddcea167bbfcfe00cbd5f865eae59
Dan_Alboteanu <alboteanud@gmail.com>
2020-11-07 23:37:12 +0200

    Initial Commit

---
+++ Bavaria - Covid state/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bavaria - Covid state
+//
+//  Created by Dan Alboteanu on 07/11/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
